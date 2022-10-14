537446207d9c470f68ffc577781c178959c5b5f9
isg <ivan7sg@gmail.com>
2022-10-14 12:07:16 -0500

    listas

--- 04_lists.py
+++ 04_lists.py
@@ -5,4 +5,15 @@ from audioop import lin2adpcm
 
 
 my_list = list()
-my_other_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [15, 24, 68, 53, 30, 31]
+print(my_list)
+print(len(my_list))
+
+my_other_list = [35, 1.77, 'Iván', "SG"]
+print(type(my_other_list))
+
+print
